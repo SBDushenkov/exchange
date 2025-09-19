a88d9ff5d7d34f91e870da448faf385069e15929
SBDushenkov <papa@PC2020>
2025-09-20 02:52:17 +0300

    1

---
+++ src/Utils.java
@@ -0,0 +1,15 @@
+package ru.dushenkov;
+
+public abstract class Utils {
+    public static void print(Object obj) {
+        System.out.print(obj);
+    }
+
+    public static void println(Object obj) {
+        System.out.println(obj);
+    }
+
+    public static void printErr(Object obj) {
+        System.err.println(obj);
+    }
+}
